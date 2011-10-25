8f975a1dc78341e38546bff5baeecce258b3524b
Jon <jonathan.ballard@gmail.comm>
2011-10-24 20:31:38 -0400

    More Logging. (6)

--- cookbooks/ssh_tunnel/recipes/default.rb
+++ cookbooks/ssh_tunnel/recipes/default.rb
@@ -52,7 +52,7 @@ tunnel_vars = {
 # should be set up on
 
 puts "Logging in via SSH [ Pre Node Instance ]"
-puts "[Framework] #{node[:engineyard].keys}"
+puts "[Framework] #{node[:engineyard][:apps]}"
 
 
 if node[:engineyard][:environment][:apps][:framework_env] == 'sandbox'
